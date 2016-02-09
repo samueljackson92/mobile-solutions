baeb974837b36acb55f911b0f9097bdf7dffa1c8
Samuel Jackson <samueljackson@outlook.com>
2016-02-09 22:20:14 +0000

    Update path

--- assignment1/deploy.sh
+++ assignment1/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd ./src
 phonegap build android
 echo 'Uninstalling app'
 adb uninstall uk.ac.aber.dcs.slj11.conference
 echo 'Reinstalling...'
-adb -s f2963c9 install platforms/android/build/outputs/apk/android-debug.apk
-cd ..
+adb -s f2963c9 install src/platforms/android/build/outputs/apk/android-debug.apk
